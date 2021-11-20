c33a06f3aec07aa2179056c482e9fdbd7b77844f
jenadiusnicholaus <ucode888@gmail.com>
2021-11-20 12:12:01 +0300

    fixing ckeditor issue

--- intmainblog/settings.py
+++ intmainblog/settings.py
@@ -87,12 +87,12 @@ WSGI_APPLICATION = 'intmainblog.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # if DEBUG:
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # else:
 DATABASES = {
     'default': {
