4e4ddaf94a4aeb224fc48b2fd5a8de63c254a44b
jenadiusnicholaus <ucode888@gmail.com>
2021-11-20 13:46:19 +0300

    update , updated the  static files

--- intmainblog/settings.py
+++ intmainblog/settings.py
@@ -156,7 +156,7 @@ MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "blog", "static"),
-    os.path.join(BASE_DIR, "static"),
+
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
